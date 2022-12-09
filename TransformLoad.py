63e81c81d7f2e6e6af80c413c891d1aac81b6748
tca93 <tca93@sfu.ca>
2022-12-08 17:58:44 -0800

    added utests and classification results for limited dataset

--- TransformLoad.py
+++ TransformLoad.py
@@ -7,19 +7,35 @@ from scipy import fft
 import os
 from scipy import stats
 import seaborn
-
-
 seaborn.set()
+
+OUTPUT_TEMPLATE = (
+    'Shoes vs No-Shoes Frequencies U-Test p-value: {shoes_feet}\n'
+    'Uphill vs Downhill Frequencies U-Test p-value: {up_down}\n'
+    'Flat-surface vs Uphill Frequencies U-Test p-value: {flat_up}\n'
+    'Flat-surface vs Downhills Frequencies U-Test p-value: {flat_down}\n'
+    'Adult vs Senior Frequencies U-Test p-value: {adult_senior}\n'
+)
+
 
 uphill = './Uphill_data'
 downhill = './Downhill_data'
 flatsurface_withshoes = './flat_withshoes_data'
 flatsurface_noshoes = './flat_noshoes_data'
-
-data_folders = ['./flat_withshoes_data', './flat_noshoes_data' ]
-
+senior_flat_data = './Senior_flat_data'
 
 
+def loadSeniorFlatData():
+    folder = os.fsencode(senior_flat_data)
+    filenames = []
+    for file in os.listdir(folder):
+        filename = os.fsdecode(file)
+        if filename.endswith(('.csv')):
+            filenames.append(senior_flat_data + '/' + filename)
+  
+    frequencies = processDataFiles(filenames, senior_flat_data)
+    # print(frequencies)
+    return frequencies
 
 
 def loadFlatWalkingShoesData():
@@ -31,7 +47,7 @@ def loadFlatWalkingShoesData():
             filenames.append(flatsurface_withshoes + '/' + filename)
   
     frequencies = processDataFiles(filenames, flatsurface_withshoes)
-    print(frequencies)
+    # print(frequencies)
     return frequencies
     
 
@@ -44,7 +60,7 @@ def loadFlatWalkingWithoutShoesData():
             filenames.append(flatsurface_noshoes + '/' + filename)
   
     frequencies = processDataFiles(filenames, flatsurface_noshoes)
-    print(frequencies)
+    # print(frequencies)
     return frequencies
 
 
@@ -58,7 +74,7 @@ def loadUphillData():
             filenames.append(uphill + '/' + filename)
   
     frequencies = processDataFiles(filenames, uphill)
-    print(frequencies)
+    # print(frequencies)
     return frequencies
 
 
@@ -72,7 +88,7 @@ def loadDownhillData():
             filenames.append(downhill + '/' + filename)
   
     frequencies = processDataFiles(filenames, downhill)
-    print(frequencies)
+    # print(frequencies)
     return frequencies
     
 
@@ -85,7 +101,7 @@ def loadFlatWalkingWithoutShoesData():
             filenames.append(flatsurface_noshoes + '/' + filename)
   
     frequencies = processDataFiles(filenames, flatsurface_noshoes)
-    print(frequencies)
+    # print(frequencies)
     return frequencies
 
     
@@ -109,14 +125,15 @@ def processDataFiles(filenames, outputName):
     for count, item in enumerate(filenames):
           
         data = pd.read_csv(item)
-    
         data = data[data['time'] > 2]
+        data = data[data['time'] < (data['time'].iloc[-1] - 3)]
     
         linearCombination = data["aT (m/s^2)"]
         b, a = signal.butter(3, 0.1, btype='lowpass', analog=False)
         low_passed = signal.filtfilt(b, a, linearCombination)
         data['low_passed'] = low_passed
-    
+        
+        ## show plot of low pass.
     
         #https://stackoverflow.com/questions/4225432/how-to-compute-frequency-of-data-using-fft
     
@@ -138,100 +155,40 @@ def processDataFiles(filenames, outputName):
     npArray = np.asarray(frequencies)
     return npArray
 
+def writeFrequenciesToFile(freq_array, walking_type):
+    df = pd.DataFrame()
+    df['frequencies'] = freq_array
+    df['walking_type'] = walking_type
+    df.to_csv(walking_type + '.csv', index = False)
 
 shoes_frequencies = loadFlatWalkingShoesData()
 feet_frequencies = loadFlatWalkingWithoutShoesData()
-
-
-plt.hist(feet_frequencies, bins=10)
-
 uphill_frequencies = loadUphillData()
 downhill_frequencies = loadDownhillData()
+senior_frequencies = loadSeniorFlatData()
 
-print(stats.mannwhitneyu(shoes_frequencies, feet_frequencies).pvalue)
-print(stats.mannwhitneyu(uphill_frequencies, downhill_frequencies).pvalue)
-
-    
-# for index, folder_name in enumerate(data_folders):
-#     loadFileNamesWithPath(folder_name,folder_name)
-#     print('\n')
-
-# for index, folder in enumerate(data_folders):
-#     for file in os.listdir(folder):
-#         filename = os.fsdecode(file)
-#         if filename.endswith(('.csv')):
-#             filenames.append(folder + '/' + filename)
-#     print (filenames)
-    
-#     for count, item in enumerate(filenames):
-        
-#         data = pd.read_csv(item)
-    
-#         data = data[data['time'] > 2]
-    
-#         linearCombination = data["aT (m/s^2)"]
-#         b, a = signal.butter(3, 0.1, btype='lowpass', analog=False)
-#         low_passed = signal.filtfilt(b, a, linearCombination)
-#         data['low_passed'] = low_passed
-    
-    
-#         #https://stackoverflow.com/questions/4225432/how-to-compute-frequency-of-data-using-fft
-    
-#         data['fftx'] = fft.fft(low_passed)  # perform fourier transform
-#         data['fftx'] = fft.fftshift(data['fftx']) # shifts the data so the frequency of 0 is centered.
-#         data['fftx'] = abs(data['fftx']) # take absolute values
-    
-#         first_sample = data['time'].iloc[0]
-#         last_sample = data['time'].iloc[-1]
-#         num_samples = len(data)
-    
-#         dt = round(num_samples/(last_sample - first_sample))
-    
-    
-#         data['freq'] = np.linspace(-dt/2, dt/2, num = len(data))
-    
-#         data = data[data['freq'] > 0.1] 
-        
-#         row = data.iloc[data['fftx'].argmax()]
-#         print(row['freq'])
-
-    # plt.plot(data['freq'], data['fftx'])
-    
-
-# data = pd.read_csv(sys.argv[1])
-
-# data = data[data['time'] > 2]
-
-# linearCombination = data["aT (m/s^2)"]
-
-# b, a = signal.butter(3, 0.1, btype='lowpass', analog=False)
-# low_passed = signal.filtfilt(b, a, linearCombination)
-# data['low_passed'] = low_passed
-
-# plt.plot(data['time'], data['low_passed'])
+writeFrequenciesToFile(shoes_frequencies, 'flat')
+writeFrequenciesToFile(uphill_frequencies, 'uphill')
+writeFrequenciesToFile(downhill_frequencies, 'downhill')
+writeFrequenciesToFile(feet_frequencies, 'noshoes')
 
 
-# #https://stackoverflow.com/questions/4225432/how-to-compute-frequency-of-data-using-fft
-
-# data['fftx'] = fft.fft(low_passed)  # perform fourier transform
-# data['fftx'] = fft.fftshift(data['fftx']) # shifts the data so the frequency of 0 is centered.
-# data['fftx'] = abs(data['fftx']) # take absolute values
-
-# first_sample = data['time'].iloc[0]
-# last_sample = data['time'].iloc[-1]
-# num_samples = len(data)
-
-# dt = round(num_samples/(last_sample - first_sample))
+plt.hist(shoes_frequencies, bins=10)
+plt.hist(feet_frequencies, bins=10)
+plt.hist(uphill_frequencies, bins=10)
+plt.hist(downhill_frequencies, bins=10)
+plt.hist(senior_frequencies, bins=10)
+plt.title('Histogram of frequencies')
 
 
-# data['freq'] = np.linspace(-dt/2, dt/2, num = len(data))
-
-# data = data[data['freq'] > 0.1] 
-
-# plt.plot(data['freq'], data['fftx'])
-
-
-
+  
+print(OUTPUT_TEMPLATE.format(
+shoes_feet = stats.mannwhitneyu(shoes_frequencies, feet_frequencies).pvalue,
+up_down = stats.mannwhitneyu(uphill_frequencies, downhill_frequencies).pvalue,
+flat_up = stats.mannwhitneyu(shoes_frequencies, uphill_frequencies).pvalue,
+flat_down = stats.mannwhitneyu(shoes_frequencies, downhill_frequencies).pvalue,
+adult_senior = stats.mannwhitneyu(feet_frequencies, senior_frequencies).pvalue
+))
 
 
 
